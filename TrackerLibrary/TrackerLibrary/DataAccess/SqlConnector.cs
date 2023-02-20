5d85ddba01dece363680c2d1371732e1e11c2c0e
Szymon253191 <sedward154@gmail.com>
2023-02-21 00:23:04 +0100

    Altered SQL script; Modified connectionString to proper server; Created function 'Create Prize' that was tested and works.

--- TrackerLibrary/TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/TrackerLibrary/DataAccess/SqlConnector.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -6,21 +7,39 @@ using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Model;
 
+
+	//@PlaceNumber int,
+	//@PlaceName nvarchar(50),
+	//@PrizeAmount money,
+    //@PrizePercentage float,
+    //@id int = 0 output
+
 namespace TrackerLibrary.DataAccess
 {
     public class SqlConnector : IDataConnection
     { 
         /// TODO - Make the CreatePrize method to do what it should actually.
         /// <summary>
-        /// Saves a new prize to the database
+        /// Saves a new prize to the database.
         /// </summary>
-        /// <param name="model">The prize information</param>
+        /// <param name="model">The prize information.</param>
         /// <returns>The prize information, including the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournament")))
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments")))
             {
+                var p = new DynamicParameters();
+                p.Add("@PlaceNumber", model.PlaceNumber);
+                p.Add("@PlaceName", model.PlaceName);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
+                p.Add("@id", 0, dbType:DbType.Int32, direction: ParameterDirection.Output);
 
+                connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+
+                model.Id = p.Get<int>("@id");
+
+                return model;
             }
         }
     }
