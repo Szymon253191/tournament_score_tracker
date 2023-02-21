528f9054b8de286de70a8f289e8a7a76f0fb04b8
Szymon253191 <sedward154@gmail.com>
2023-02-21 13:11:17 +0100

    Added connector to TextFile database for prizeModel

--- TrackerLibrary/TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Model;
+using TrackerLibrary.DataAccess.TextHelpers;
+using System.Runtime.InteropServices;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+
         /// TODO - Make the CreatePrize method to do what it should actually.
         /// <summary>
         /// Saves a new prize to the database
@@ -17,7 +21,24 @@ namespace TrackerLibrary.DataAccess
         /// <returns>The prize information, including the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // Load the textfile and convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // Find the ID
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+            // Add the new record with the new ID (max + 1)
+            prizes.Add(model);
+
+            // Convert the prizes to list<sting> and save the list <string> to the text file
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
